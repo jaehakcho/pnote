cbd159a22c5b9e001bfb0118affc64c451057422
privatenote <whwogkra@naver.com>
2015-08-20 15:09:14 +0000

    라우트 지정하기

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Testapp::Application.routes.draw do
+Mvc::Application.routes.draw do
+  get 'home/hello' => 'home#hello'
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+  ...
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
